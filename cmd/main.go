c9b4e6b62b914368c8602fec8c2cdd9916582ed6
f-murphy <100728686+f-murphy@users.noreply.github.com>
2024-07-25 21:35:12 +0300

    add logrus

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"log"
 	"req3rdPartyServices/handler"
 	"req3rdPartyServices/repository"
 	"req3rdPartyServices/service"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
  	db, err := sqlx.Connect("postgres", "postgres://postgres:qwerty@localhost:5436/postgres?sslmode=disable")
 	if err != nil {
-		log.Fatal("failed to initialize db: ", err.Error())
+		logrus.Fatal("failed to initialize db: ", err.Error())
 	}
 
 	repos := repository.NewTaskRepository(db)
